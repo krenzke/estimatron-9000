bf4efa0cd62b61d47f6f571342d9ea17da4f7180
Tom Krenzke <tom.krenzke@hyfn.com>
2015-09-01 14:37:21 -0700

    more responses

--- app.rb
+++ app.rb
@@ -21,17 +21,20 @@ class App < Sinatra::Base
 
   def generate_phrase
     numbers = [2,4,8,12,16]
-    case rand(5)
+    people = ['Burton', 'Krenzke', 'Scheirman', 'Greg']
+    case rand(6)
     when 0
       "Let's just call it a #{numbers.sample} for now"
     when 1
-      "Whatever Burton says it is"
+      "Whatever #{people.sample} says it is"
     when 2
       "#{numbers.sample}-ish"
     when 3
       "No way that's anything #{['more','less'].sample} than a #{numbers.sample}"
     when 4
       "Give it a #{numbers.sample}, but don't assign the ticket to me"
+    when 5
+      "Call is a #{numbers.sample}, but assign a #{numbers.sample} to the rest of them"
     end
   end
 end
