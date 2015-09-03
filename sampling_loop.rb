720e9e30f9b8f21d9b167a8212771ae8a8224b3c
Tom Krenzke <tom.krenzke@hyfn.com>
2015-09-02 20:38:49 -0700

    foo

--- sampling_loop.rb
+++ sampling_loop.rb
@@ -2,6 +2,7 @@ require './metrics_sampler'
 require './rabbit_mq_producer'
 require './service_discovery'
 require 'json'
+require 'logger'
 
 class SamplingLoop
   include RabbitMqProducer
@@ -9,12 +10,15 @@ class SamplingLoop
   def intialize
     @sampler = MetricsSampler.new
     @service_info = ServiceDiscovery.info
+    @logger = Logger.new(STDOUT)
   end
 
   def run
+    logger.info("SAMPLING_LOOP#run")
     @exchange ||= mq_create_exchange('services_status', :fanout)
     Thread.new {
       1000.times {
+        logger.info("SAMPLING_LOOP#sample")
         s = @sampler.sample.merge(@service_info)
         @exchange.publish(s.to_json, content_type: 'application/json')
         sleep 5
